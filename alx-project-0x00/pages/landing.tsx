be374070b9f6db2af5521d411bc62fbae053615d
Benjamin Appiah <ben.appiah451@gmail.com>
2025-07-16 16:48:38 +0000

    Advanced Task Done

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -10,7 +10,7 @@ const Landing: React.FC = () => {
             <div className="flex-gap-4 mt-4">
                 <h1 className="font-semibold text-lg mb-3">Button Styles</h1>
                 <div className="flex gap-4">
-                    <Button title="Rounded Full" styles="rounded-full" />
+                    <Button title="Rounded Full" styles="rounded-lg" />
                     <Button title="Rounded Medium" styles="rounded-md" />
                     <Button title="Rounded Small" styles="rounded-sm" />
                 </div>
